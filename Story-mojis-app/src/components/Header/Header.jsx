95d7f2c0db0a8459ed6eaaf8b066fab78c604af2
ALLYSON FELIPPO BREDA DE MONTE CHRISTO <allyson_christo@estudante.sesisenai.org.br>
2025-02-12 22:13:49 -0300

    first commit

---
+++ Story-mojis-app/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='container-header'>
+      <h1>Story Cubes Game</h1>
+    </div>
+  )
+}
+
+export default Header
