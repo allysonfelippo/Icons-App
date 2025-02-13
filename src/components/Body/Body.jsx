63c0f61f3cffdda3755a92105ef90efd64a71327
Allyson Felippo <allysonfelippo35@gmail.com>
2025-02-13 02:48:41 -0300

    Imagens de tres quadrados mudam com o botao

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,14 @@
+
+import Game from '../Game/Game'
+import './Body.css'
+
+
+function Body() {
+  return (
+    <div className='container-body'>
+      <Game />
+    </div>
+  )
+}
+
+export default Body
---
+++ src/components/Game/Game.jsx
@@ -0,0 +1,51 @@
+
+import { useState } from 'react';
+import './Game.css'
+import {
+    GiAxolotl,
+    GiCapybara,
+    GiCat,
+    GiCirclingFish,
+    GiCrocJaws,
+    GiCruiser,
+    GiCrownedSkull,
+    GiCurledTentacle,
+    GiCzSkorpion,
+    GiElephant
+} from "react-icons/gi";
+
+function Game() {
+    const icons = [
+        <GiAxolotl />, <GiCapybara />, <GiCat />,
+        <GiCirclingFish />, <GiCrocJaws />, <GiCruiser />,
+        <GiCrownedSkull />, <GiCurledTentacle />, <GiCzSkorpion />,
+        <GiElephant />
+    ]
+
+    const [icon, setIcon] = useState([<GiAxolotl />,
+    <GiCapybara />,
+    <GiCat />])
+
+    function TrocarIcone() {
+        const newIcons = icons.map((() => {
+            const randomIcon = Math.floor(Math.random() * icons.length)
+            return icons[randomIcon];
+        }));
+        setIcon(newIcons);
+
+    }
+    return (
+        <div className='container-main-content'>
+            <div className='container-cubes'>
+                <div className='cube-style-content icon-size'>{icon[0]}</div>
+                <div className='cube-style-content icon-size'>{icon[1]}</div>
+                <div className='cube-style-content icon-size'>{icon[2]}</div>
+            </div>
+            <div className='container-btn'>
+                <button onClick={TrocarIcone} className='style-btn'>ROLL AGAIN</button>
+            </div>
+        </div>
+    )
+}
+
+export default Game
